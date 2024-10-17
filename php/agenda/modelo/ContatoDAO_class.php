e5a93d47392895dc86e85246d5cfd13dfb7f50df
IgoorSouza <igor.castro@estudante.iftm.edu.br>
2024-10-16 22:07:09 -0300

    contatoDAO + listaContato

--- php/agenda/modelo/ContatoDAO_class.php
+++ php/agenda/modelo/ContatoDAO_class.php
@@ -25,8 +25,33 @@
                 $stmt->execute();
                 // $this->con->close();
                 // $this->con = null;
-            } catch (PDOException e) {
-                echo "Erro: " . e->getMessage();
+            } catch (PDOException $e) {
+                echo "Erro: " . $e->getMessage();
+            }
+        }
+
+        public function listar($query = null) {
+            try {
+                if ($query == null) {
+                    $dados = $this->con->query("SELECT * FROM contato");
+                } else {
+                    $dados = $this->con->query($query);
+                }
+
+                $lista = array();
+
+                foreach ($dados as $linha) {
+                    $c = new Contato();
+                    $c->setId($linha["id"]);
+                    $c->setNome($linha["nome"]);
+                    $c->setTelefone($linha["telefone"]);
+                    $c->setEmail($linha["email"]);
+                    $lista[] = $c;
+                }
+
+                return $lista;
+            } catch (PDOException $e) {
+                echo "Erro: " . $e->getMessage();
             }
         }
     }
---
+++ php/agenda/visao/listaContato.php
@@ -0,0 +1,42 @@
+<!DOCTYPE html>
+<html lang="pt-br">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Listagem de Contatos</title>
+</head>
+<body>
+    <?php 
+        if (isset($status)) {
+            echo "<h2>" . $status .  "</h2>"; 
+        }
+    ?>
+
+    <a href="contato.php?fun=cadastrar">Cadastrar</a>
+    <br /> <br />
+
+    <table border="1px">
+        <tr>
+            <th>ID</th>
+            <th>Nome</th>
+            <th>Email</th>
+            <th>Telefone</th>
+            <th><img src="visao/img/update.png" width="30px" /></th>
+            <th><img src="visao/img/delete.png" width="30px" /></th>
+        </tr>
+
+        <?php
+            foreach ($lista as $c) {
+                echo "<tr>";
+                echo "<td>" . $c->getId() . "</td>";
+                echo "<td><a href='contato.php?fun=exibir&id=" . $c->getId() . "'>" . $c->getNome() . "</a></td>";
+                echo "<td>" . $c->getEmail() . "</td>";
+                echo "<td>" . $c->getTelefone() . "<td>";
+                echo "<td><a href='contato.php?fun=alterar&id=" . $c->getId() . "'> <img src='visao/img/update.png' width='30' /> </a></td>";
+                echo "<td><a href='contato.php?fun=excluir&id=" . $c->getId() . "'> <img src='visao/img/delete.png' width='30' /> </a></td>";
+                echo "</tr>";
+            }
+        ?>
+    </table>
+</body>
+</html>
